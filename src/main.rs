fba3f3bcd5ecc943c0c9f34cf952abbd0668bb75
Mateusz Ryczek <mateusz@u.northwestern.edu>
2025-02-06 18:54:20 -0600

    OS PATH reading and processing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+fn get_path() -> String {
+    match std::env::var("PATH") {
+        Ok(val) => val,
+        Err(e) => format!("couldn't interpret $PATH: {e}"),
+    }
+}
+
+fn path2vec(path: String) -> Vec<String> {
+    path.split(":")
+        .map(String::from)
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let waypoints = path2vec(get_path());
+
+    for waypoint in waypoints {
+        println!("{}", waypoint);
+    }
 }
